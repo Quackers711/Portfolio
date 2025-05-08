cfbc31bc856fc7356225e0ccda4083d3d14691fb
Quackers711 <quackduck711@gmail.com>
2025-05-08 07:49:40 +0200

    First

---
+++ portfolio/astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+import partytown from '@astrojs/partytown';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), partytown(), storyblok({
+    accessToken: env.STORYBLOK_API_KEY,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+    },
+    apiOptions: {
+      region: 'eu',
+    },
+  })],
+  vite: {
+    plugins: [basicSsl()]
+  },
+  adapter: vercel(),
+});
---
+++ portfolio/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Quack711 Portfolio';
+export const SITE_DESCRIPTION = 'Cybersecurity and Software Development Portfolio';
